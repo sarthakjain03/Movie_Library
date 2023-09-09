c9179e6e64560f05c5120ccaad2487c5f5f0781b
Sarthak Jain <jainsarthak2003@gmail.com>
2023-09-09 23:18:03 +0530

    basic files

---
+++ src/context/ContextProvider.js
@@ -0,0 +1,20 @@
+import React, { useContext, createContext, useState } from 'react'
+
+const StateContext = createContext()
+
+export const ContextProvider = ({children}) => {
+    
+
+    return (
+        <StateContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+
+}
+
+export const useStateContext = () => useContext(StateContext)
