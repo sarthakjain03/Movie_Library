7c73715c51fc45c70ef815bc8c7f1b8198446757
Sarthak Jain <jainsarthak2003@gmail.com>
2023-09-26 12:31:54 +0530

    unavailable poster added

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const img_500 = "https://image.tmdb.org/t/p/w500"
 export const img_300 = "https://image.tmdb.org/t/p/w300"
+
+export const unavailable = "https://www.movienewz.com/img/films/poster-holder.jpg"
